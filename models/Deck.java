98a965840380781c6ea02dddc5ea1c04a59671ff
6831503087-SawKehBlu <6831503087@lamduan.mfu.ac.th>
2026-03-29 19:37:59 +0700

    Update Deck.java
    Implementing shuffleDeck() and drawCard()

--- models/Deck.java
+++ models/Deck.java
@@ -19,12 +19,24 @@ public class Deck {
     }
 
     private void shuffleDeck() {
-        // TODO: Shuffle cards
+        Random rand = new Random();
+
+        for (int i = cards.size() - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1); 
+
+            
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(j));
+            cards.set(j, temp);
+    }
     }
 
     public Card drawCard() {
-        // TODO: Return top card
-        return null;
+        if (drawPile.isEmpty()) {
+            System.out.println("Deck is empty!");
+            return null;
+        }
+        return drawPile.poll();
     }
 
     public boolean isEmpty() {
